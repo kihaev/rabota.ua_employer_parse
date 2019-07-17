a64807ba201697282fb3b4213632b59b493ee2e0
Yura Kikhaiev <50831818+kihaev@users.noreply.github.com>
2019-07-17 21:52:23 +0300

    Add files via upload

--- rabota_ua_updated.py
+++ rabota_ua_updated.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
-import pickle
+import pickle 
 import sqlite3
 
 
@@ -14,7 +14,7 @@ def conn_db(path):
 
 # create table
 def create_table_db(name):
-    create_table = "CREATE TABLE " + name + " (Name_Surname text, Profile_Url text, Email text, Phone_Number text, City text, CV_Url text)"
+    create_table = "CREATE TABLE " + name + " (Ad_Name text, Ad_Url text, Price text, Phone_Number text, Breed text)"
     try:
         mycursor.execute(create_table)
     except sqlite3.OperationalError:
@@ -28,10 +28,10 @@ def drop_table_db(name):
     mycursor.execute(drop_table)
     
     
-# insert people info into db
-def insert_into_db(Name_surname, Profile_url, Email, Phone_number, City, Cv_url):
-    sql = "INSERT INTO employees (Name_Surname, Profile_Url, Email, Phone_Number, City, CV_Url) VALUES (?, ?, ?, ?, ?, ?)"
-    val = (Name_surname, Profile_url, Email, Phone_number, City, Cv_url)
+# insert animal info into db
+def insert_into_db(table_name, Ad_name, Ad_url, Price, Phone_number, Breed):
+    sql = "INSERT INTO "+table_name+" (Ad_Name, Ad_Url, Price, Phone_Number, Breed) VALUES (?, ?, ?, ?, ?)"
+    val = (Ad_name, Ad_url, Price, Phone_number, Breed)
     mycursor.execute(sql, val)
     conn.commit()
     
@@ -51,41 +51,34 @@ def load_cookie(path):
             driver.add_cookie(cookie)
             
             
-# parsing people info
-def get_people_info(start_page):
+# parsing animals info
+def get_animals_info(start_page, table_name):
     #find last page number
     driver.get(start_page)
     soup_page = BeautifulSoup(driver.page_source, "html.parser")
-    num_page = soup_page.find("div", {"id":"ctl00_centerZone_employerResumeList_grVwResume_ctl24_pagerInnerTable"})
-    last_page = int(num_page.find_all("dd")[-1].a.get_text())
+    num_page = soup_page.find("div", {"class":"pager rel clr"})
+    last_page = int(num_page.find_all("span")[-3].get_text())
     #check all pages
     for page in range(1, last_page + 1):
         driver.get(start_page + "&pg=" + str(page))
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        #check all profiles
-        for profile in soup.find_all("a", class_="rua-p-t_16 rua-p-c-default ga_cv_view_cv"):
-            profile_url = profile["href"]
-            name_surname = profile.text
-            driver.get(profile_url)
-            soup_profile = BeautifulSoup(driver.page_source, "html.parser")
-            #print(soup_profile.prettify())
-            try:
-                email = soup_profile.find("span", {"class":"rua-p-t_13", "id":"ctl00_centerZone_BriefResume1_ViewAttachedCV1_cvHeader_lblEmailValue"}).get_text()
-            except AttributeError:
-                email = soup_profile.find("span", {"class":"rua-p-t_13", "id":"ctl00_centerZone_BriefResume1_CvView1_cvHeader_lblEmailValue"}).get_text()
-            try:
-                phone_number = soup_profile.find("span", {"class":"rua-p-t_13", "id":"ctl00_centerZone_BriefResume1_CvView1_cvHeader_lblPhoneValue"}).get_text()
-            except AttributeError:
-                phone_number = ""
-            try:
-                city = soup_profile.find("span", {"class":"rua-p-t_13", "id":"ctl00_centerZone_BriefResume1_CvView1_cvHeader_lblRegionValue"}).get_text()
-            except AttributeError:
-                city = ""  
-            cv_url = str(soup.find("a", class_="rua-p-t_16")["href"])
-            #driver.get(cv_url)
-            #print(name_surname + "\n" + profile_url + "\n" + email +"\n"+ phone_number + "\n" + city + "\n" + cv_url)
+        #check all advertisements
+        for ad_num, ad in enumerate(soup.find_all("a", class_="marginright5 link linkWithHash detailsLink")[5:]):
+            ad_url = ad["href"]
+            price = soup.find_all("p", {"class":"price"})[ad_num + 5].text.strip()
+            ad_name = ad.text.strip()
+            driver.get(ad_url)
+            sleep(5)
+            #getting phone number
+            elem = driver.find_element_by_xpath('//*[@id="contact_methods"]/li[2]/div')
+            elem.click()
+            sleep(5)
+            soup_ad = BeautifulSoup(driver.page_source, "html.parser")
+            phone_number = soup_ad.find("strong",{"class":"xx-large"}).text
+            breed = soup_ad.find_all("td", {"class":"value"})[1].text.strip()
+            print(ad_name + "\n" + ad_url +"\n"+ price +"\n"+ phone_number + "\n" + breed)
             #inserting info
-            insert_into_db(name_surname, profile_url, email, phone_number, city, cv_url)
+            insert_into_db(table_name, ad_name, ad_url, price, phone_number, breed)
 
                 
 # show results
@@ -97,22 +90,28 @@ def show_table_rows(name):
 
 #start                        
 def start():
-    create_table_db(table_name)
-    #sleep(30)
+    create_table_db(table_name_dogs)
+    create_table_db(table_name_cats)
+    #sleep(300)
     #pickle.dump(driver.get_cookies() , open("cookies.pkl","wb"))
     load_cookie(cookies_path)
-    get_people_info(starting_page)
-    show_table_rows(table_name)  
+    get_animals_info(starting_page_dogs, table_name_dogs)
+    get_animals_info(starting_page_cats, table_name_cats)
+    show_table_rows(table_name_dogs)
+    show_table_rows(table_name_cats)
+
     
-    
-#constants   
-database_path = "mydatabase.db"
-table_name = "employees"
+#constants    
+database_path = "mydatabase_animals.db"
+table_name_cats = "cats"
+table_name_dogs = "dogs"
 driver_path = "C:\\Users\\yurak\\Desktop\\test\\geckodriver.exe"
-cookies_path = "C:\\Users\\yurak\\Desktop\\test\\cookies.pkl"
-starting_page = "https://notebook.rabota.ua/employer/notepad/cvs?vacancyId=-1"
+cookies_path = "cookies.pkl"
+starting_page_dogs = "https://www.olx.ua/zhivotnye/sobaki/kiev/q-Печерский/?search%5Border%5D=created_at%3Adesc"
+starting_page_cats = "https://www.olx.ua/zhivotnye/koshki/kiev/q-Печерский/?search%5Border%5D=created_at%3Adesc"
+authorization_page = "https://www.olx.ua/account/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index"
 conn, mycursor = conn_db(database_path)
-driver = sel_init(driver_path, starting_page)
+driver = sel_init(driver_path, authorization_page)
 
 
 start()
